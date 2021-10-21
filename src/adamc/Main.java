8cd748aef31bb2fc91d6414f8cd7e2107f5b2419
Adam Caine <adam.caine10@gmail.com>
2021-10-21 23:18:56 +0100

    Modified main class to use javaFX and added simple hello world window

--- src/adamc/Main.java
+++ src/adamc/Main.java
@@ -1,11 +1,18 @@
 package adamc;
 
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 import java.util.Scanner;
 
 /**
  * The main class of our program, where the program starts.
  */
-public class Main {
+public class Main extends Application {
 
   /**
    * The main function, where our program starts, asks the user for a path to the file we want
@@ -14,6 +21,7 @@ public class Main {
    * @param args The arguments that the user can pass through to our program from the command line.
    */
   public static void main(String[] args) {
+    launch(args);
     System.out.println("Please enter the path of the program you would like to run" +
         " (relative to the programs folder of this program");
     Scanner scanner = new Scanner(System.in);
@@ -21,5 +29,15 @@ public class Main {
     FileHandler handler = new FileHandler("programs/" + path);
     InstructionHandler iHandler = new InstructionHandler();
     iHandler.start(handler);
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    stage.setScene(new Scene(createContent(), 300, 300));
+    stage.show();
+  }
+
+  private Parent createContent() {
+    return new StackPane(new Text("Hello World"));
   }
 }
